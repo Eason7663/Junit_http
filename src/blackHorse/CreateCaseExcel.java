c7b312261afc0ae3617901b24df69cce0fa7b3c1
Eason.ZHANG <zhyj663@163.com>
2017-06-05 22:32:41 +0800

    Signed-off-by: Eason.ZHANG <zhyj663@163.com>

--- src/blackHorse/CreateCaseExcel.java
+++ src/blackHorse/CreateCaseExcel.java
@@ -7,6 +7,7 @@ public class CreateCaseExcel {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ExcelOperator excelOperator = new ExcelOperator(".\\dat\\test case.xlsx");
+		ExcelOperator excelOperator = new ExcelOperator(".\\dat\\test case.xlsx");
 	}
 
 }
