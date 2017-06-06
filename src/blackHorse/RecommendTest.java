f89dbc8427cce40c6af28fee8f746eaeff9f7b33
Eason.ZHANG <zhyj663@163.com>
2017-06-06 18:23:58 +0800

    Signed-off-by: Eason.ZHANG <zhyj663@163.com>

--- src/blackHorse/RecommendTest.java
+++ src/blackHorse/RecommendTest.java
@@ -10,6 +10,8 @@ import org.junit.Test;
 import net.sf.json.JSONObject;
 
 public class RecommendTest {
+	private String userName = null;
+	
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -27,11 +29,11 @@ public class RecommendTest {
 	@Test
 	public void test1() {
 		String s=HttpRequest.sendGet("http://10.15.144.72/dzh/blackhorse/recommend", 
-				"usertoken=C33kH2UwVqQ0ZcTG5bg4b0eNXA8ngq74b7djvCw5XSc.1496732782&pool=多周期共振策略&token=00000002:2359591317:debdf92aca398eb500e85819cd560ae4a9249d47");
-		JSONObject ss;
-        System.out.println(s);
-        System.out.println(s);
+				"usertoken=H52DYemO7bAUtbE9DBWfostrSDtH*nR1VQmv-*F8P5A.1496749135&pool=多周期共振策略&token=00000002:2359591317:debdf92aca398eb500e85819cd560ae4a9249d47");
+		JSONObject ss = JSONObject.fromObject(s);
 
+		
+		
 //        //发送 POST 请求z
 //        String sr=HttpRequest.sendPost("https://hao.360.cn", "key=123&v=456");
 //        System.out.println(sr);
---
+++ src/blackHorse/UserToken.java
@@ -0,0 +1,31 @@
+package blackHorse;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
+import net.sf.json.JSONObject;
+import netscape.javascript.JSObject;
+
+public class UserToken {
+	private String UserName = null;
+	
+	
+	
+	public UserToken() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static String getUserToken(String userName) {
+		StringBuilder stringBuilder = new StringBuilder("appId=3&reqId=3-`uuidgen`&userIp=10.15.54.204&exp=3600&userId=");
+		stringBuilder.append(userName);
+		JSONObject jsObject = JSONObject.fromObject(HttpRequest.sendGet("http://10.15.107.167/gettoken",stringBuilder.toString())) ;
+		
+		
+		return (String) jsObject.get("token");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(UserToken.getUserToken("ycltest003"));
+	}
+
+}
